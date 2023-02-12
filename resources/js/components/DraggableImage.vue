297f361b680c9b6e14b5397b59309418ff278a58
Alexander Faus <alexfaus08@gmail.com>
2023-02-12 16:52:50 -0500

    comments

--- resources/js/components/DraggableImage.vue
+++ resources/js/components/DraggableImage.vue
@@ -38,19 +38,26 @@ function enableMoving(event) {
         const rect = img.value.getBoundingClientRect();
         const proposedY = rect.y - newY;
         const proposedX = rect.x - newX;
+
         if (proposedY < canvasTop) {
+            // if it tried to go above the canvas, set it to the canvas top
             img.value.style.top = canvasTop + 'px';
         } else if (proposedY + img.value.offsetHeight > canvasTop + canvasHeight) {
+            // if it tried to go below the canvas, set it to the canvas bottom
             img.value.style.top = canvasTop + canvasHeight - img.value.offsetHeight + 'px';
         } else {
+            // otherwise, let it go where it wants!
             img.value.style.top = proposedY + 'px';
         }
 
         if (proposedX < canvasLeft) {
+            // if it tried to go to the left of the canvas, set it to the canvas left
             img.value.style.left = canvasLeft;
         } else if (proposedX + img.value.offsetWidth > canvasLeft + canvasWidth) {
+            // if it tried to go to the right of the canvas, set it to the canvas right
             img.value.style.left = canvasLeft + canvasWidth - img.value.offsetWidth + 'px';
         } else {
+            // otherwise, let it go where it wants!
             img.value.style.left = proposedX + 'px';
         }
 
