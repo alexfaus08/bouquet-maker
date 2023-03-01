f25b3839ecc963877ecd6dbb3d30b7c7879593e1
Alexander Faus <alexfaus08@gmail.com>
2023-03-01 16:36:10 -0500

    add types

--- resources/js/components/DraggableImage.vue
+++ resources/js/components/DraggableImage.vue
@@ -5,7 +5,7 @@
     @mousedown="enableMoving"
   />
 </template>
-<script setup>
+<script setup lang="ts">
 import {ref, defineProps} from 'vue';
 
 const props = defineProps({
@@ -16,9 +16,9 @@ const props = defineProps({
     }
 });
 
-const img = ref(null);
+const img = ref<HTMLElement | null>(null);
 
-function enableMoving(event) {
+function enableMoving(event: MouseEvent) {
     const canvasHeight = props.canvas.offsetHeight;
     const canvasWidth = props.canvas.offsetWidth;
     const canvasTop = props.canvas.offsetTop;
@@ -29,37 +29,37 @@ function enableMoving(event) {
 
     let prevX = event.clientX;
     let prevY = event.clientY;
-    const imgHeight = img.value.offsetHeight;
-    const imgWidth = img.value.offsetWidth;
+    const imgHeight = img.value?.offsetHeight;
+    const imgWidth = img.value?.offsetWidth;
 
-    function moveElement(event) {
+    function moveElement(event: MouseEvent) {
         const newX = prevX - event.clientX;
         const newY = prevY - event.clientY;
 
-        const rect = img.value.getBoundingClientRect();
-        const proposedY = rect.y - newY;
-        const proposedX = rect.x - newX;
+        const rect = img.value?.getBoundingClientRect();
+        const proposedY = rect!.y - newY;
+        const proposedX = rect!.x - newX;
 
         if (proposedY < canvasTop) {
             // if it tried to go above the canvas, set it to the canvas top
-            img.value.style.top = canvasTop + 'px';
-        } else if (proposedY + imgHeight > canvasTop + canvasHeight) {
+            img.value!.style.top = canvasTop + 'px';
+        } else if (proposedY + imgHeight! > canvasTop + canvasHeight) {
             // if it tried to go below the canvas, set it to the canvas bottom
-            img.value.style.top = canvasTop + canvasHeight - imgHeight + 'px';
+            img.value!.style.top = canvasTop + canvasHeight - imgHeight! + 'px';
         } else {
             // otherwise, let it go where it wants!
-            img.value.style.top = proposedY + 'px';
+            img.value!.style.top = proposedY + 'px';
         }
 
         if (proposedX < canvasLeft) {
             // if it tried to go to the left of the canvas, set it to the canvas left
-            img.value.style.left = canvasLeft;
-        } else if (proposedX + imgWidth > canvasLeft + canvasWidth) {
+            img.value!.style.left = canvasLeft + 'px';
+        } else if (proposedX + imgWidth! > canvasLeft + canvasWidth) {
             // if it tried to go to the right of the canvas, set it to the canvas right
-            img.value.style.left = canvasLeft + canvasWidth - imgWidth + 'px';
+            img.value!.style.left = canvasLeft + canvasWidth - imgWidth! + 'px';
         } else {
             // otherwise, let it go where it wants!
-            img.value.style.left = proposedX + 'px';
+            img.value!.style.left = proposedX + 'px';
         }
 
         prevX = event.clientX;
