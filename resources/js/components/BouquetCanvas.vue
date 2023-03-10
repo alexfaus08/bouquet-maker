4a8b8a6150b2dcd2c10b7017a322681a6b08dbeb
Alexander Faus <alexfaus08@gmail.com>
2023-03-10 16:40:36 -0500

    feat: resize and drag with interactjs

--- resources/js/components/BouquetCanvas.vue
+++ resources/js/components/BouquetCanvas.vue
@@ -2,16 +2,18 @@
   <div class="h-screen">
     <div
       ref="canvas"
-      class="mx-auto my-10 h-5/6 w-1/2 rounded border-4 border-pink-500 bg-pink-100"
+      class="canvas mx-auto my-10 h-5/6 w-1/2 rounded border-4 border-pink-500 bg-pink-100"
     >
-      <DraggableImage :canvas="canvas" />
+      <!--      <DraggableImage :canvas="canvas" />-->
+      <DraggableWithInteractJS />
     </div>
   </div>
 </template>
 
 <script setup>
-import DraggableImage from './DraggableImage.vue';
+// import DraggableImage from './DraggableImage.vue';
 import {ref} from 'vue';
+import DraggableWithInteractJS from './DraggableWithInteractJS.vue';
 
 const canvas = ref(null);
 </script>
---
+++ resources/js/components/DraggableWithInteractJS.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="resize-drag box-border h-32 w-32 bg-pink-600" />
+</template>
+
+<script setup lang="ts">
+import interact from 'interactjs';
+function dragMoveListener(event) {
+    const target = event.target;
+    // keep the dragged position in the data-x/data-y attributes
+    const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+    const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+        target.style.transform =
+            'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+}
+interact('.resize-drag')
+    .resizable({
+        // resize from all edges and corners
+        edges: {left: true, right: true, bottom: true, top: true},
+
+        listeners: {
+            move(event) {
+                const target = event.target;
+                let x = (parseFloat(target.getAttribute('data-x')) || 0);
+                let y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+                // update the element's style
+                target.style.width = event.rect.width + 'px';
+                target.style.height = event.rect.height + 'px';
+
+                // translate when resizing from top or left edges
+                x += event.deltaRect.left;
+                y += event.deltaRect.top;
+
+                target.style.transform = 'translate(' + x + 'px,' + y + 'px)';
+
+                target.setAttribute('data-x', x);
+                target.setAttribute('data-y', y);
+                target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+            }
+        },
+        modifiers: [
+            // keep the edges inside the parent
+            interact.modifiers.restrictEdges({
+                outer: 'parent'
+            }),
+
+            // minimum size
+            interact.modifiers.restrictSize({
+                min: {width: 100, height: 50}
+            })
+        ],
+
+        inertia: true
+    })
+    .draggable({
+        listeners: {move: dragMoveListener},
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent'
+            })
+        ]
+    });
+</script>
+
+<style scoped>
+
+</style>
